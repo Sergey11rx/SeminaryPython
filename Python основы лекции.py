93781f6decaeda7338ba7f13c386983faea723e3
Sergey <gyraserg@mail.ru>
2023-05-27 15:42:49 +0300

    first commit

---
+++ Python основы лекции.py
@@ -0,0 +1,124 @@
+numbers = [2, 5, 13, 7, 6, 4]
+size = 6
+sum = 0
+avg = 0
+index = 0
+
+while (index < size):
+   sum = sum + numbers[index] 
+   index = index + 1
+else:
+   avg = sum / size
+print(avg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# array = [20, 59, 121, 34, 56, 64]
+# i = 0
+# size = len(array)
+
+# while (i < size // 3):
+#     if (x = array[i]):
+#         array[i] = array[size - 1 - i]
+#         array[size - 1] = x
+#     i = i + 1
+
+# print (array)
+
+
+
+
+
+
+#start = 1
+#end = 100
+#sum = 0
+ 
+
+#if start <= end:
+    #sum = sum + start 
+   # start = start + 1
+#start = sum
+#start <= end
+#print(sum)
+
+
+
+
+
+
+#a = 3
+#b = 5
+#c = 12
+#d = 30
+#sum = a + b + c + d
+#print (sum / 4)
+
+
+
+
+
+#if holiday == True:
+    #print ('holiday')
+#else:
+    #print ('work')
+
+
+
+
+
+
+
+#a = 2
+#b = 10
+#c = 34
+
+#if a > b:
+    #if a > c:
+        #print ('max =', a)
+    #else:
+        #print ('max =', c)
+#elif b > c:
+       # print ('max =', b)
+    #else:
+       # print ('max =', c)
+
+
+
+
+
+#a = 3
+#b = 12
+#c = 13
+#d = 34
+#e = 54
+#max = a
+
+#if max < b:
+    #max = b
+#if max < c:
+   # max = c
+#if max < d:
+    #max = d
+#if max < e:
+    #max = e
+    
+#print (max)
